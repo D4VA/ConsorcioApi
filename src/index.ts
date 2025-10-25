2c8e102a651c3269d1308cf1cae104bfd0bd1968
DAV4 <diegovelasquezavila@gmail.com>
2025-10-25 15:55:17 -0600

    set up to deploy in vercel

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import 'dotenv/config';
-import app from './app';
+import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== "production") {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
