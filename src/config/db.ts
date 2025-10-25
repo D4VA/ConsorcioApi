94a813b717e2fe76a740b8b3a51c0fcdf7a061c6
DAV4 <diegovelasquezavila@gmail.com>
2025-10-25 17:29:20 -0600

    mvp

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-import { execSync } from "child_process";
-
-// Si no existe el cliente generado, lo generamos en runtime
-try {
-  require.resolve(".prisma/client");
-} catch (e) {
-  console.log("🧩 Generando cliente Prisma en runtime...");
-  execSync("npx prisma generate", { stdio: "inherit" });
-}
 
 export const prisma = new PrismaClient();
